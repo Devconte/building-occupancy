42df12cb17b5e40a5f5abb08e8d88e8173bc72b3
Devconte <romain.lcnte@gmail.com>
2024-06-17 16:13:16 +0200

    feat : added stat card component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <script>
 export default {
   name: "App",
-  components: {},
+  components: {
+    StatCard: () => import("./components/StatCardComponent.vue"),
+  },
+  data() {
+    return {
+      statCards: [
+        {
+          title: "nombre d'étages",
+          value: "3",
+        },
+        { title: "nombre de pièces", value: "57" },
+        { title: "pourcentage d'occupation", value: "80%" },
+      ],
+    };
+  },
 };
 </script>
 <template>
   <div id="app">
     <header class="h-16 w-full"></header>
     <main class="container mx-auto">
-      <h1 class="text-[#ffac49]">
-        Statistiques d'occupation du batiment: [NOM DU BATIMENT]
+      <h1 class="text-[#ffac49] text-center mb-6 text-3xl font-bold">
+        [NOM DU BATIMENT]
       </h1>
-      <div class="floor-container">
-        <p>[Nom Etage] occupé à [78%]</p>
-        <div class="roomList">
-          <div class="room">
-            <p>[Room 1]</p>
-            <p>[Status]</p>
-          </div>
-          <div class="room">
-            <p>Room 1</p>
-            <p>Status</p>
-          </div>
-        </div>
+      <div class="flex justify-evenly">
+        <stat-card
+          :key="index"
+          v-for="(card, index) in statCards"
+          :title="card.title"
+          :value="card.value"
+        ></stat-card>
       </div>
     </main>
   </div>
---
+++ src/components/StatCardComponent.vue
@@ -0,0 +1,29 @@
+<template>
+  <div
+    class="h-52 w-52 bg-gray-200 rounded-xl shadow-sm flex items-center justify-center flex-col py-4"
+  >
+    <div class="font-semibold text-sm text-amber-600">{{ title }}</div>
+
+    <div
+      class="flex-1 flex justify-center items-center text-8xl slashed-zero tabular-nums"
+    >
+      {{ value }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "StatCardComponent",
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    value: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
