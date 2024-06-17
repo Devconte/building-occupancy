572e612db29210b15cd11a926c06ae19decce6d6
Devconte <romain.lcnte@gmail.com>
2024-06-17 21:07:53 +0200

    improve style

--- src/components/TableListComponent.vue
+++ src/components/TableListComponent.vue
@@ -5,7 +5,7 @@
     <thead class="w-full px-2">
       <tr class="flex justify-between items-center">
         <th
-          class="text-xl font-extrabold"
+          class="text-xl font-extra-bold"
           v-for="(title, index) in titles"
           :key="index"
         >
@@ -17,7 +17,7 @@
       <tr
         v-for="(row, index) in rows"
         :key="index"
-        class="flex w-full px-2 justify-between items-center hover:bg-amber-300 hover:cursor-pointer transition-all duration-300"
+        class="flex w-full px-2 justify-between items-center hover:bg-amber-300 hover:cursor-pointer transition-all duration-300 h-10"
         :class="index % 2 === 0 ? 'bg-slate-100' : 'bg-slate-200'"
       >
         <td v-for="(name, index) in row" :key="index">{{ name }}</td>
